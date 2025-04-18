dcf6fb40af9646d8e035e10ede6d4a95701b421c
juangomezn <gomezninoj681@gmail.com>
2025-04-17 22:06:19 -0500

    feat: :rocket: Sesion 2 Variables

---
+++ 01-variables.js
@@ -0,0 +1,26 @@
+//var -> Manera inicial de declarar variables No recomendada de usar
+
+var helloWorld = "!Hola, JavaScript!"
+console.log(helloWorld)
+
+helloWorld = "!Hola de nuevo, JavaScript!"
+console.log(helloWorld)
+
+/*let -> Se pueden llamar a ejecucion a traves de un console log pero 
+despues de ser inicializada ya que de no ser asi daran error*/
+
+let helloWorld2 = "!Hola, JavaScript 2!"
+console.log(helloWorld2)
+
+helloWorld2 = "!Hola de nuevo, JavaScript 2!"
+console.log(helloWorld2)
+
+/*const -> Se declaran variables constantes, es decir, no se pueden cambiar 
+su valor luego de ser inicializadas*/
+
+const helloWorld3 = "!Hola, JavaScript 3!"
+console.log(helloWorld3)
+
+//Error ya que una constante no puede cambiar su valor
+/*helloWorld3 = "!Hola de nuevo, JavaScript 3!" 
+console.log(helloWorld3)*/
