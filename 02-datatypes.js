c9d712ac9c90899340007fffa654879d3a2bcc12
juangomezn <gomezninoj681@gmail.com>
2025-04-17 22:07:41 -0500

    feat: :rocket: Sesion 3 Datatypes

---
+++ 02-datatypes.js
@@ -0,0 +1,48 @@
+//Tipos de datos primitivos
+
+//Cadena de texto (String)
+let name = "Juan David Gomez"
+let email = 'gomezninoj681@gmail.com'
+let equipo = `Fc Barcelona`
+
+//Números (Number)
+let age = 19 //Entero
+let height = 1.65 //Decimal
+
+//Booleanos (Boolean) 
+let isStudent = true
+let isTeacher = false
+
+//Undefined
+let undefinedValue
+console.log(undefinedValue)
+
+//Null
+let nullValue = null
+
+//Symbol
+let mySymbol = Symbol("mysymbol")
+
+//BigInt
+let myBigInt = BigInt(12345678901234567890123456789)
+let myBigInt2 = 12345678901234567890123456789n
+
+//Mostramos los tipos de datos
+console.log(typeof name)
+console.log(typeof email)
+console.log(typeof equipo)
+//---------------------------
+console.log(typeof age)
+console.log(typeof height)
+//---------------------------
+console.log(typeof isStudent)
+console.log(typeof isTeacher)
+//---------------------------
+console.log(typeof undefinedValue)
+//---------------------------
+console.log(typeof nullValue)
+//---------------------------
+console.log(typeof mySymbol)
+//---------------------------
+console.log(typeof myBigInt)
+console.log(typeof myBigInt2)
